c1915bfa1906a01ce65e5dcbd36f7cc315d68153
Edson L Costa <edsonlimajll@gmail.com>
2022-03-07 21:37:11 -0300

    commit inicial da api

---
+++ src/main/java/com/api/loveanddonateapi/LoveanddonateApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.loveanddonateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveanddonateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveanddonateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/loveanddonateapi/LoveanddonateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.loveanddonateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveanddonateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
