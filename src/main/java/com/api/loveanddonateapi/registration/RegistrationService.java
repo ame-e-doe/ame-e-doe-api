961063f8c8a62a0c238473fc0ff04f71eaa19aee
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-03-25 02:04:24 -0300

    Inclui classes para criar método de registro usuários

---
+++ src/main/java/com/api/loveanddonateapi/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.api.loveanddonateapi.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest registrationRequest) {
+        return "works";
+    }
+}
