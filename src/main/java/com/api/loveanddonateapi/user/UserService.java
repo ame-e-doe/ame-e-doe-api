6faa598b51d6c5b5fa3667dec9d0c1ece7e8f180
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-03-29 02:30:25 -0300

    implementa lógica e regra de negocio cria token e ativa user

--- src/main/java/com/api/loveanddonateapi/user/UserService.java
+++ src/main/java/com/api/loveanddonateapi/user/UserService.java
@@ -1,5 +1,7 @@
 package com.api.loveanddonateapi.user;
 
+import com.api.loveanddonateapi.registration.token.ConfirmationToken;
+import com.api.loveanddonateapi.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,13 +9,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND = "user with email %s not found";
+
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername( String email )
@@ -29,6 +37,8 @@ public class UserService implements UserDetailsService {
                 .isPresent();
 
         if( userExists ) {
+//            TODO: Implementar lógica, se o e-mail já existe na base porém o usuário ainda não confirmou o token
+//            TODO: Devemos reenviar o e-mail com um token novo
             throw new IllegalStateException( "email already taken" );
         }
 
@@ -38,8 +48,23 @@ public class UserService implements UserDetailsService {
 
         userRepository.save( user );
 
-//        TODO: SEND configrmation token
+        String token = UUID.randomUUID().toString();
 
-        return "it works";
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes( 15 ),
+                user
+        );
+
+        confirmationTokenService.saveConfirmationToken( confirmationToken );
+
+//        TODO: SEND E-MAIL
+
+        return token;
+    }
+
+    public int enableAppUser( String email ) {
+        return userRepository.enableUser( email );
     }
 }
