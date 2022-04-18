674bb4bfd59901a524ddc945a03123c380f9eff8
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-04-17 21:42:25 -0300

    Ajustes importantes WebSecurityConfig e inclui secrets no application

--- src/main/java/com/api/loveanddonateapi/configuration/WebSecurityConfig.java
+++ src/main/java/com/api/loveanddonateapi/configuration/WebSecurityConfig.java
@@ -1,48 +1,66 @@
 package com.api.loveanddonateapi.configuration;
 
+import com.api.loveanddonateapi.configuration.security.jwt.AuthEntryPointJwt;
+import com.api.loveanddonateapi.configuration.security.jwt.AuthTokenFilter;
+import com.api.loveanddonateapi.dto.UserDTO;
 import com.api.loveanddonateapi.service.UserService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@AllArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity( prePostEnabled = true )
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserService userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt authEntryPointJwt;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    public void configure( AuthenticationManagerBuilder authenticationManagerBuilder ) throws Exception {
+        authenticationManagerBuilder.userDetailsService( userDetailsService ).passwordEncoder( passwordEncoder() );
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure( HttpSecurity http ) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests()
-                    .antMatchers( "/api/v*/**" )
-                    .permitAll()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin();
-    }
-
-    @Override
-    protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
-        auth.authenticationProvider(  daoAuthenticationProvider() );
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
-        provider.setPasswordEncoder( bCryptPasswordEncoder );
-        provider.setUserDetailsService( userService );
-        return provider;
+                .cors()
+                .and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint( authEntryPointJwt )
+                .and().sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+                .and().authorizeRequests().antMatchers( "/api/auth/**" ).permitAll()
+                .antMatchers( "/api/**" ).permitAll()
+                .anyRequest().authenticated();
+        http.addFilterBefore( authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class );
     }
 
 }
