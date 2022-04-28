0d1692e64d63461d374650090e4933e658d88a10
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-04-28 01:58:18 -0300

    ajusta DTO e valida regras de negocio para login

--- src/main/java/com/api/loveanddonateapi/dto/SignInDTO.java
+++ src/main/java/com/api/loveanddonateapi/dto/SignInDTO.java
@@ -1,19 +1,25 @@
 package com.api.loveanddonateapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
-@Getter
-@Setter
 @AllArgsConstructor
-@EqualsAndHashCode
 @ToString
+@Getter
+@JsonInclude( JsonInclude.Include.NON_NULL )
 public class SignInDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private final String email;
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
     private final String password;
 
 }
--- src/main/java/com/api/loveanddonateapi/service/SignInService.java
+++ src/main/java/com/api/loveanddonateapi/service/SignInService.java
@@ -1,11 +1,14 @@
 package com.api.loveanddonateapi.service;
 
-import com.api.loveanddonateapi.security.jwt.JwtUtils;
 import com.api.loveanddonateapi.dto.SignInDTO;
+import com.api.loveanddonateapi.dto.response.MessageResponse;
 import com.api.loveanddonateapi.models.User;
+import com.api.loveanddonateapi.repository.UserRepository;
+import com.api.loveanddonateapi.security.jwt.JwtUtils;
 import com.api.loveanddonateapi.security.response.JwtResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +17,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,20 +30,39 @@ public class SignInService {
     @Autowired
     JwtUtils jwtUtils;
 
+    @Autowired
+    UserRepository userRepository;
+
     public ResponseEntity< ? > auth( SignInDTO signInDTO ) {
+
+        Boolean isEnabled = userRepository.isEnabled( signInDTO.getEmail() );
+
+        if( Objects.equals( isEnabled, false ) ) {
+            return ResponseEntity
+                    .status( HttpStatus.UNAUTHORIZED )
+                    .body( new MessageResponse( "Error: User is not enabled, please activate your account" ) );
+        } else if( !userRepository.existsByEmail( signInDTO.getEmail() ) ) {
+            return ResponseEntity
+                    .status( HttpStatus.UNAUTHORIZED )
+                    .body( new MessageResponse( "Error: User does not exist" ) );
+        }
+        return authenticated( signInDTO );
+    }
+
+    private ResponseEntity< ? > authenticated( SignInDTO signInDTO ) {
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken( signInDTO.getEmail(), signInDTO.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
+                new UsernamePasswordAuthenticationToken( signInDTO.getEmail(), signInDTO.getPassword() ) );
+        SecurityContextHolder.getContext().setAuthentication( authentication );
+        String jwt = jwtUtils.generateJwtToken( authentication );
 
         User user = ( User ) authentication.getPrincipal();
-        List<String> roles = user.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect( Collectors.toList());
-        return ResponseEntity.ok(new JwtResponse(jwt,
+        List< String > roles = user.getAuthorities().stream()
+                .map( item -> item.getAuthority() )
+                .collect( Collectors.toList() );
+        return ResponseEntity.ok( new JwtResponse( jwt,
                 user.getId(),
                 user.getUsername(),
-                roles));
+                roles ) );
     }
 
 }
