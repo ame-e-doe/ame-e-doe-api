04f5c61190aa0eafad69ac67276315408e9cecdd
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-05-06 14:11:56 -0300

    Inclui Slf4j para DigitalProductService

--- src/main/java/com/api/loveanddonateapi/service/DigitalProductService.java
+++ src/main/java/com/api/loveanddonateapi/service/DigitalProductService.java
@@ -8,6 +8,7 @@ import com.api.loveanddonateapi.models.User;
 import com.api.loveanddonateapi.repository.CategoryRepository;
 import com.api.loveanddonateapi.repository.DigitalProductRepository;
 import com.api.loveanddonateapi.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class DigitalProductService {
 
@@ -30,6 +32,7 @@ public class DigitalProductService {
     private ModelMapper mapper = new ModelMapper();
 
     public List< DigitalProductDTO > getAllProducts() {
+        log.debug( "Find all products: " );
         return this.digitalProductRepository.findAll()
                 .stream()
                 .map( products -> mapper.map( products, DigitalProductDTO.class ) )
@@ -37,12 +40,14 @@ public class DigitalProductService {
     }
 
     public DigitalProductDTO getProductById( Long idProduct ) {
+        log.debug( "Find product by id {}", idProduct );
         return this.digitalProductRepository.findById( idProduct )
                 .map( product -> mapper.map( product, DigitalProductDTO.class ) )
                 .orElseThrow( () -> new EntityNotFoundException( "Produto de id: " + idProduct + " não encontrado." ) );
     }
 
     public List< DigitalProductDTO > getAllProductsByCategory( Long idCategory ) {
+        log.debug( "Find all products by id category {}", idCategory );
         Category category = categoryRepository.findById( idCategory )
                 .orElseThrow( () -> new EntityNotFoundException( "Categoria de id: " + idCategory + " não encontrado." ) );
         return this.digitalProductRepository.getDigitalProductsByCategory( category )
@@ -52,12 +57,14 @@ public class DigitalProductService {
     }
 
     public DigitalProductsByUserDTO getAllProductsByUser( Long idUser ) {
+        log.debug( "Find products by id user {}", idUser );
         User user = this.userRepository.findById( idUser )
                 .orElseThrow( () -> new EntityNotFoundException( "Usuario de id: " + idUser + " não encontrado." ) );
         return mapper.map( user, DigitalProductsByUserDTO.class );
     }
 
     public List< DigitalProductDTO > getAllProductsBySearch( String search ) {
+        log.debug( "Find all products by search {}", search );
         return this.digitalProductRepository
                 .findDigitalProductsByTitleOrDescription( search )
                 .stream()
