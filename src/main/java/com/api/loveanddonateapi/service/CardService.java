853ab8521d559b993544c2dd61a2e0d97d14721c
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-05-06 14:11:06 -0300

    Inclui Slf4j para CardService.java

--- src/main/java/com/api/loveanddonateapi/service/CardService.java
+++ src/main/java/com/api/loveanddonateapi/service/CardService.java
@@ -6,6 +6,7 @@ import com.api.loveanddonateapi.dto.CardDTO;
 import com.api.loveanddonateapi.exception.EntityNotFoundException;
 import com.api.loveanddonateapi.repository.CardRepository;
 import com.api.loveanddonateapi.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class CardService {
 
@@ -25,11 +27,13 @@ public class CardService {
 
     private final ModelMapper mapper = new ModelMapper();
 
-    public CardDTO createCard( CardDTO cardDto, Long userId ) {
-        Card card = mapper.map( cardDto, Card.class );
+    public CardDTO createCard( CardDTO cardDTO, Long userId ) {
+        Card card = mapper.map( cardDTO, Card.class );
 
+        log.debug( "Given create card find user by id {}", userId );
         Optional< User > user = userRepository.findById( userId );
 
+        log.debug( "If user exists save card for id user {}", userId );
         if( user.isPresent() ) {
             card.setUser( user.get() );
             return mapper.map( cardRepository.save( card ), CardDTO.class );
@@ -38,6 +42,7 @@ public class CardService {
     }
 
     public List< CardDTO > getAllCards( Long userId ) {
+        log.debug( "Find all cards for user with userId {}", userId );
         return cardRepository.findAllCardsByUserId( userId )
                 .stream()
                 .map( card -> mapper.map( card, CardDTO.class ) )
@@ -45,6 +50,7 @@ public class CardService {
     }
 
     public void deleteCardById( Long cardId ) {
+        log.debug( "Delete card by cardId {}", cardId );
         cardRepository.delete( cardRepository.findById( cardId )
                 .orElseThrow( () -> new EntityNotFoundException( "Cartão de id: " + cardId + " não encontrado." ) ) );
     }
