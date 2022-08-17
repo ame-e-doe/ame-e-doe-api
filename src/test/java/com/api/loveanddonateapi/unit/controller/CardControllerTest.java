24673b50ce19571d42063898be53870ba05e7322
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-08-17 18:14:59 -0300

    feat: unit tests config include and ci config

---
+++ src/test/java/com/api/loveanddonateapi/unit/controller/CardControllerTest.java
@@ -0,0 +1,18 @@
+package com.api.loveanddonateapi.unit.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles( "test" )
+public class CardControllerTest {
+
+    @Test
+    @DisplayName( "roda teste de teste" )
+    public void helloTest() {
+        System.out.println("Teste rolando");
+    }
+
+}
