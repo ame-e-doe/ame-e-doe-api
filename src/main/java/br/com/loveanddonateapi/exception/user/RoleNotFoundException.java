35dedadd0cb6af87c0e8f5fb45fc9657a481ccfa
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-10-06 14:20:38 -0300

    feat: ajuste packs e application

--- src/main/java/br/com/loveanddonateapi/exception/user/RoleNotFoundException.java
+++ src/main/java/br/com/loveanddonateapi/exception/user/RoleNotFoundException.java
@@ -1,4 +1,4 @@
-package br.com.loveanddonateapi.exception;
+package br.com.loveanddonateapi.exception.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
