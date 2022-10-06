7e057767ed828cbf35095c84cb41faa14358ca0a
Rennan Costa <rennansilvacosta@hotmail.com>
2022-10-05 21:43:24 -0300

    dinamica carrinho de compras finalizada

--- src/main/java/br/com/loveanddonateapi/controller/CartController.java
+++ src/main/java/br/com/loveanddonateapi/controller/CartController.java
@@ -1,5 +1,6 @@
 package br.com.loveanddonateapi.controller;
 
+import br.com.loveanddonateapi.models.Cart;
 import br.com.loveanddonateapi.service.CartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,11 +18,21 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    @ApiOperation(value = "Adicionar um produto ao carrinho de compras.")
+    @ApiOperation(value = "Adiciona um produto ao carrinho de compras.")
     @PostMapping("add-product/{productId}")
-    public ResponseEntity<Void> addCartItem(@RequestHeader("Authorization") String token, @PathVariable Long productId) {
-        this.cartService.addCartItem(token, productId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Cart> addCartItem(@RequestHeader("Authorization") String token, @PathVariable Long productId) {
+        return new ResponseEntity<>(this.cartService.addCartItem(token, productId), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Recupera o carrinho de compras do usuario logado.")
+    @GetMapping()
+    public ResponseEntity<Cart> getCartByUser(@RequestHeader("Authorization") String token) {
+        return new ResponseEntity<>(this.cartService.getCartByUser(token), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Remove um item do carrinho de compras.")
+    @DeleteMapping("{cartItemId}")
+    public ResponseEntity<Cart> removeCartItem(@RequestHeader("Authorization") String token, @PathVariable Long cartItemId) {
+        return new ResponseEntity<>(this.cartService.removeCartItem(token, cartItemId), HttpStatus.OK);
+    }
 }
--- src/main/java/br/com/loveanddonateapi/repository/CartRepository.java
+++ src/main/java/br/com/loveanddonateapi/repository/CartRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     @Query(value = "SELECT c FROM Cart c WHERE c.user.id = :userId")
-    Cart getCartByUserId(Long userId);
+    Optional<Cart> getCartByUserId(Long userId);
 
 }
