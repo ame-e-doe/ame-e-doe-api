07b86443bb38756c0dfe1b7b00a3638ac274188f
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-10-11 11:43:03 -0300

    ajuste: Ajuste para atender a mudanca de pacotes de algumas classes de config e security

--- src/main/java/br/com/loveanddonateapi/service/CardService.java
+++ src/main/java/br/com/loveanddonateapi/service/CardService.java
@@ -6,7 +6,7 @@ import br.com.loveanddonateapi.exception.EntityNotFoundException;
 import br.com.loveanddonateapi.models.Card;
 import br.com.loveanddonateapi.models.User;
 import br.com.loveanddonateapi.repository.CardRepository;
-import br.com.loveanddonateapi.security.jwt.JwtUtils;
+import br.com.loveanddonateapi.configuration.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
