5517fadca673a9d7958ed8a0eca2777cdc9c426a
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-10-24 17:10:24 -0300

    ref: ajuste em imports AuthController

--- src/main/java/br/com/loveanddonateapi/controller/AuthController.java
+++ src/main/java/br/com/loveanddonateapi/controller/AuthController.java
@@ -4,7 +4,7 @@ import br.com.loveanddonateapi.dto.user.SignInDTO;
 import br.com.loveanddonateapi.service.SignInService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +12,12 @@ import javax.validation.Valid;
 
 @CrossOrigin( origins = "*", maxAge = 3600 )
 @RestController
+@RequiredArgsConstructor
 @RequestMapping( "/api/auth" )
 @Api(tags = {"Autenticação"})
 public class AuthController {
 
-    @Autowired
-    SignInService signInService;
+    private final SignInService signInService;
 
     @PostMapping( "/login" )
     @ApiOperation( value = "Realiza login")
