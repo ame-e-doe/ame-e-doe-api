9c40581a2e026170391124b6d89540bfd33158f8
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-10-24 17:12:43 -0300

    ref: refactor para Card e inclui logs

--- src/main/java/br/com/loveanddonateapi/repository/CardRepository.java
+++ src/main/java/br/com/loveanddonateapi/repository/CardRepository.java
@@ -9,17 +9,17 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
+public interface CardRepository extends JpaRepository< Card, Long > {
 
     @Override
     Card save( Card card );
 
     @Override
-    Optional<Card> findById(Long id);
+    Optional< Card > findById( Long id );
 
-    List<Card> findCardsByUserId( Long idUser );
+    List< Card > findCardsByUserId( Long idUser );
 
-    @Query(value = "SELECT c FROM Card c WHERE c.cardNumber = :cardNumber AND c.user.id = :idUser")
-    Optional<Card> findCardByCardNumberAndUser(String cardNumber, Long idUser);
+    @Query( value = "SELECT c FROM Card c WHERE c.cardNumber = :cardNumber AND c.user.id = :idUser" )
+    Optional< Card > findCardByCardNumberAndUser( String cardNumber, Long idUser );
 
 }
